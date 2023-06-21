86e052cb9426a5b8c4db9ca06310aacfe81c4d37
Igor Smolkov <smolkov.igor@gmail.com>
2023-06-21 18:59:49 +0700

    db name

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,13 @@ import { getTodos } from '@/lib/todos'
 import styles from './page.module.css'
 
 export default async function Home() {
-  const { todos } = await getTodos()
+  const { todos } = await getTodos();
+  console.log(todos);
 
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <ul className='mt-4 flex flex-col gap-1'>
+        <ul>
           {todos?.map(todo => (
             <p key={todo.id}>{todo.name}</p>
           ))}
--- lib/todos.ts
+++ lib/todos.ts
@@ -2,9 +2,14 @@ import prisma from './prisma'
 
 export async function getTodos() {
   try {
+    console.log('getTodosstart');
+    // console.log(prisma);
+    console.log('afprisma');
     const todos = await prisma.users.findMany()
+    console.log('getTodos', todos);
     return { todos }
   } catch (error) {
+    console.log(error);
     return { error }
   }
 }
