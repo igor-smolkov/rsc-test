75a1be8a00810852478168d5f2ce9532d54e32ed
Igor Smolkov <smolkov.igor@gmail.com>
2023-06-22 18:13:27 +0700

    add tmp

--- lib/text.ts
+++ lib/text.ts
@@ -1,5 +1,5 @@
 import fs from 'fs';
-import path from 'path';
+// import path from 'path';
 import util from 'util';
 
 const readFile = util.promisify(fs.readFile);
@@ -15,8 +15,8 @@ export async function readText() {
 
 export async function readText2() {
   try {
-    const p = path.join(process.cwd(), 'public/');
-    const text = await readFile(`${p}text2.txt`, 'utf-8');
+    // const p = path.join(process.cwd(), 'public/');
+    const text = await readFile('/tmp/text2.txt', 'utf-8');
     return { text }
   } catch (error) {
     return { error }
